548c50e3337892ae68aba355664c2607f0806cfd
Christopher Conceição Moura <ccmoura@Christophers-MacBook-Air.local>
2023-10-22 22:52:44 -0300

    389_find_the_difference

---
+++ python/389_find_the_difference.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        s_letters_occurrences = {}
+
+        for i in s:
+            if s_letters_occurrences.get(i) != None:
+                s_letters_occurrences[i] += 1
+            else:
+                s_letters_occurrences[i] = 1
+        
+        for i in t:
+            occurrence = s_letters_occurrences.get(i)
+            if occurrence == None or occurrence == 0:
+                return i
+            else:
+                s_letters_occurrences[i] -= 1
+
+print(Solution().findTheDifference("", "y"))
+        
