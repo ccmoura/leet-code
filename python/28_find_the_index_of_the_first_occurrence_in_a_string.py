08a4b0752ade897f10fb5af5faaf2590c091a8f4
Christopher Conceição Moura <ccmoura@Christophers-MacBook-Air.local>
2023-11-02 22:55:35 -0300

    use KMP to search string occurrences

--- python/28_find_the_index_of_the_first_occurrence_in_a_string.py
+++ python/28_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -1,16 +1,42 @@
 class Solution(object):
+    def compute_lps_array(self, needle):
+        needle_len = len(needle)
+        lps = [0] * needle_len
+        i = 1
+        length = 0
+
+        while i < needle_len:
+            if needle[i] == needle[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
+
     def strStr(self, haystack, needle):
         haystack_len = len(haystack)
+        needle_len = len(needle)
+        lps = self.compute_lps_array(needle)
 
-        for i, _ in enumerate(haystack):
-            if haystack[i] == needle[0]:
-                if haystack_len >= len(needle):
-                    for j, _ in enumerate(needle):
-                        if haystack[i + j] != needle[j]:
-                            break
-                        if len(needle) == j + 1:
-                            return i
-            haystack_len -= 1
+        i = 0
+        j = 0
+
+        while (haystack_len - i) >= (needle_len - j):
+            if needle[j] == haystack[i]:
+                j += 1
+                i += 1
+            
+            if j == needle_len:
+                return i - j
+            elif i < haystack_len and needle[j] != haystack[i]:
+                if j != 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
         return -1
 
-    
